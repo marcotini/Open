51e20f4505c5c629b12a807c3deb4f3144ebb67b
Marco Tini <marcotini29@gmail.com>
2021-03-21 09:53:33 +0100

    Initial Commit

---
+++ Open/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Open
+//
+//  Created by marco on 19/03/21.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate, UIScrollViewDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.webView.scrollView.delegate = self
+        
+        let myURL = URL(string:"https://www.open.online")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        webView.allowsLinkPreview = false
+        
+        // disabilita il pinch to zoom
+        webView.scrollView.delegate = self
+
+    }
+    
+    //MARK: - UIScrollViewDelegate
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+             scrollView.pinchGestureRecognizer?.isEnabled = false
+    }
+}
